dec4b731ba6e0283cb7dfb70e7b3d03dd2481ff9
Renee Hlozek <renee.hlozek@gmail.com>
2021-08-13 15:17:22 -0400

    preliminary script to download 2pt files

---
+++ pull_data_2pt_correlation_wget.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ~~~~~~~ 2pt-correlation ~~~~~~~~
+
+wget -O SDSS_spextral_galaxies_z0.1to0.2_wide.csv http://www.dunlap.utoronto.ca/~hlozek/CMBS4School/SDSS_spextral_galaxies_z0.1to0.2_wide.csv
+
